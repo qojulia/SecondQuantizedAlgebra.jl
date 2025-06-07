7ed58a7bf009f54827c561a2039e0be1b461e78d
Documenter.jl <documenter@juliadocs.github.io>
2025-06-07 17:08:48 +0000

    delete history

---
+++ benchmarks/data.js
@@ -0,0 +1,128 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1749286258579,
+  "repoUrl": "https://github.com/qojulia/SecondQuantizedAlgebra.jl",
+  "entries": {
+    "Benchmark Results": [
+      {
+        "commit": {
+          "author": {
+            "email": "orjan.ameye@hotmail.com",
+            "name": "Orjan Ameye",
+            "username": "oameye"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d6919ec324dc3bc12a4123e38c0544434771429b",
+          "message": "feat: add SW benchmark (#12)",
+          "timestamp": "2025-06-04T12:10:39+02:00",
+          "tree_id": "1cff0e602f1c170b0d793a6dee52ab41a68e6f7a",
+          "url": "https://github.com/qojulia/SecondQuantizedAlgebra.jl/commit/d6919ec324dc3bc12a4123e38c0544434771429b"
+        },
+        "date": 1749032228861,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "Jaynes–Cummings/First order Schrieffer-Wolf",
+            "value": 176499,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=56000\nallocs=1287\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":10,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "orjan.ameye@hotmail.com",
+            "name": "Orjan Ameye",
+            "username": "oameye"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3edacea833534cf4c641bd6a2d65a036f2dd69c8",
+          "message": "docs: highlight that the quantumCumulant issues in comments (#13)",
+          "timestamp": "2025-06-04T12:25:27+02:00",
+          "tree_id": "ec3f6e018819a98e0ba0051600374d65bc7e7353",
+          "url": "https://github.com/qojulia/SecondQuantizedAlgebra.jl/commit/3edacea833534cf4c641bd6a2d65a036f2dd69c8"
+        },
+        "date": 1749032794886,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "Jaynes–Cummings/First order Schrieffer-Wolf",
+            "value": 178857.5,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=56000\nallocs=1287\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":10,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "orjan.ameye@hotmail.com",
+            "name": "Orjan Ameye",
+            "username": "oameye"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bf9761917f6350fec38b0a30f30c54f31328f1ed",
+          "message": "build: tag release (#19)",
+          "timestamp": "2025-06-04T16:13:02+02:00",
+          "tree_id": "8b4e2d4a3881be855591705b0de447a7964b1b60",
+          "url": "https://github.com/qojulia/SecondQuantizedAlgebra.jl/commit/bf9761917f6350fec38b0a30f30c54f31328f1ed"
+        },
+        "date": 1749046688069,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "Jaynes–Cummings/First order Schrieffer-Wolf",
+            "value": 183675,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=56000\nallocs=1287\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":10,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "orjan.ameye@hotmail.com",
+            "name": "Orjan Ameye",
+            "username": "oameye"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7f1dad756790f7c480072d854e2c280a24e34ac8",
+          "message": "build: remove MTK (#20)",
+          "timestamp": "2025-06-07T10:49:14+02:00",
+          "tree_id": "b0b3e919e8989ce14ddb2dc3133dfe0360e4595f",
+          "url": "https://github.com/qojulia/SecondQuantizedAlgebra.jl/commit/7f1dad756790f7c480072d854e2c280a24e34ac8"
+        },
+        "date": 1749286258141,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "Jaynes–Cummings/First order Schrieffer-Wolf",
+            "value": 182521,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=51248\nallocs=1223\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":10,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
