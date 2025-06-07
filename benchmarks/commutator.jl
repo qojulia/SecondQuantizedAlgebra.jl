955c825c8711672cfc122de28ad3e0a81227fbce
Orjan Ameye <orjan.ameye@hotmail.com>
2025-06-07 16:21:36 +0200

    add nested commutator benchmark

---
+++ benchmarks/commutator.jl
@@ -0,0 +1,18 @@
+function nested_commutator(term, n)
+    if n == 0
+        return term
+    else
+        return commutator(term, nested_commutator(term, n - 1))
+    end
+end
+
+function benchmark_commutator!(SUITE)
+    h= FockSpace(:cavity)
+    @qnumbers a::Destroy(h)
+
+    term = a'*a
+    n=5
+
+    SUITE["Micro Benchmarks"]["Nested commutator"] = @benchmarkable nested_commutator($term, $n) seconds = 20
+    return nothing
+end
