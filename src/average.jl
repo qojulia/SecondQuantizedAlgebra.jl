3c1519b1c24a50dcef1931d8370f596d76062594
Orjan Ameye <orjan.ameye@hotmail.com>
2025-06-23 16:19:38 +0200

    format

--- src/average.jl
+++ src/average.jl
@@ -39,9 +39,12 @@ function +(a::Average, b::Average)
     end
     return SymbolicUtils.Add(CNumber, 0, Dict(a=>1, b=>1))
 end
-SymbolicUtils.Add(::Type{AvgSym}, coeff, dict; kw...) = SymbolicUtils.Add(CNumber, coeff, dict, kw...)
-SymbolicUtils.Mul(::Type{AvgSym}, coeff, dict; kw...) = SymbolicUtils.Mul(CNumber, coeff, dict, kw...)
-
+function SymbolicUtils.Add(::Type{AvgSym}, coeff, dict; kw...)
+    SymbolicUtils.Add(CNumber, coeff, dict, kw...)
+end
+function SymbolicUtils.Mul(::Type{AvgSym}, coeff, dict; kw...)
+    SymbolicUtils.Mul(CNumber, coeff, dict, kw...)
+end
 
 function acts_on(s::SymbolicUtils.Symbolic)
     if SymbolicUtils.iscall(s)
