44746028e5fcb56072d46fcd3b25244ffe31c85b
Documenter.jl <documenter@juliadocs.github.io>
2025-06-24 08:43:50 +0000

    delete history

---
+++ benchmarks/data.js
@@ -0,0 +1,308 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1750707675988,
+  "repoUrl": "https://github.com/qojulia/SecondQuantizedAlgebra.jl",
+  "entries": {
+    "Benchmark Results": [
+      {
+        "commit": {
+          "author": {
+            "email": "orjan.ameye@hotmail.com",
+            "name": "Orjan Ameye",
+            "username": "oameye"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d6919ec324dc3bc12a4123e38c0544434771429b",
+          "message": "feat: add SW benchmark (#12)",
+          "timestamp": "2025-06-04T12:10:39+02:00",
+          "tree_id": "1cff0e602f1c170b0d793a6dee52ab41a68e6f7a",
+          "url": "https://github.com/qojulia/SecondQuantizedAlgebra.jl/commit/d6919ec324dc3bc12a4123e38c0544434771429b"
+        },
+        "date": 1749032228861,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "Jaynes–Cummings/First order Schrieffer-Wolf",
+            "value": 176499,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=56000\nallocs=1287\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":10,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "orjan.ameye@hotmail.com",
+            "name": "Orjan Ameye",
+            "username": "oameye"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3edacea833534cf4c641bd6a2d65a036f2dd69c8",
+          "message": "docs: highlight that the quantumCumulant issues in comments (#13)",
+          "timestamp": "2025-06-04T12:25:27+02:00",
+          "tree_id": "ec3f6e018819a98e0ba0051600374d65bc7e7353",
+          "url": "https://github.com/qojulia/SecondQuantizedAlgebra.jl/commit/3edacea833534cf4c641bd6a2d65a036f2dd69c8"
+        },
+        "date": 1749032794886,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "Jaynes–Cummings/First order Schrieffer-Wolf",
+            "value": 178857.5,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=56000\nallocs=1287\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":10,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "orjan.ameye@hotmail.com",
+            "name": "Orjan Ameye",
+            "username": "oameye"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bf9761917f6350fec38b0a30f30c54f31328f1ed",
+          "message": "build: tag release (#19)",
+          "timestamp": "2025-06-04T16:13:02+02:00",
+          "tree_id": "8b4e2d4a3881be855591705b0de447a7964b1b60",
+          "url": "https://github.com/qojulia/SecondQuantizedAlgebra.jl/commit/bf9761917f6350fec38b0a30f30c54f31328f1ed"
+        },
+        "date": 1749046688069,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "Jaynes–Cummings/First order Schrieffer-Wolf",
+            "value": 183675,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=56000\nallocs=1287\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":10,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "orjan.ameye@hotmail.com",
+            "name": "Orjan Ameye",
+            "username": "oameye"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7f1dad756790f7c480072d854e2c280a24e34ac8",
+          "message": "build: remove MTK (#20)",
+          "timestamp": "2025-06-07T10:49:14+02:00",
+          "tree_id": "b0b3e919e8989ce14ddb2dc3133dfe0360e4595f",
+          "url": "https://github.com/qojulia/SecondQuantizedAlgebra.jl/commit/7f1dad756790f7c480072d854e2c280a24e34ac8"
+        },
+        "date": 1749286258141,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "Jaynes–Cummings/First order Schrieffer-Wolf",
+            "value": 182521,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=51248\nallocs=1223\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":10,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "orjan.ameye@hotmail.com",
+            "name": "Orjan Ameye",
+            "username": "oameye"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5e9b4ab4bc43689cef7b7ffe81ce6d40fea00059",
+          "message": "add nested commutator benchmark (#26)",
+          "timestamp": "2025-06-07T19:08:34+02:00",
+          "tree_id": "4614cf3a4adf38514b660031a55b32faffb135b4",
+          "url": "https://github.com/qojulia/SecondQuantizedAlgebra.jl/commit/5e9b4ab4bc43689cef7b7ffe81ce6d40fea00059"
+        },
+        "date": 1749316219243,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "Jaynes–Cummings/First order Schrieffer-Wolf",
+            "value": 177763,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=51248\nallocs=1223\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":10,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "Micro Benchmarks/Nested commutator",
+            "value": 40702363.5,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=19656720\nallocs=463760\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":20,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "orjan.ameye@hotmail.com",
+            "name": "Orjan Ameye",
+            "username": "oameye"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e42f720ffb1e771497e51808f2b8c1b36adb9d50",
+          "message": "fix: double average PR28  (#29)\n\n* test: add testset for double average\n\n* test: add nested testsets for spin and fock space\n\n* make `symtype(::Average) = CNumber`\n\n* format\n\n* overide Add(::Type{AvgSym}, coeff, dict; kw...)\n\n* format",
+          "timestamp": "2025-06-23T16:31:33+02:00",
+          "tree_id": "4104e862ba72712ca0bf3a26d8593c6677c4a399",
+          "url": "https://github.com/qojulia/SecondQuantizedAlgebra.jl/commit/e42f720ffb1e771497e51808f2b8c1b36adb9d50"
+        },
+        "date": 1750689295148,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "Jaynes–Cummings/First order Schrieffer-Wolf",
+            "value": 175468,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=51248\nallocs=1223\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":10,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "Micro Benchmarks/Nested commutator",
+            "value": 41894543.5,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=19656720\nallocs=463760\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":20,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "orjan.ameye@hotmail.com",
+            "name": "Orjan Ameye",
+            "username": "oameye"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "dca0386673b055c889e21278a4a71a2d874c2577",
+          "message": "fix: add MTK for Parameter handling in MTK@V9 (#30)\n\n* fix: add MTK for Parameter handling in MTK@V9\n\n* format\n\n* fix: compat ModelingToolkit dependency to version 9",
+          "timestamp": "2025-06-23T17:05:27+02:00",
+          "tree_id": "1db07c384d692f7a3615db991e5b1bd98cff2cf9",
+          "url": "https://github.com/qojulia/SecondQuantizedAlgebra.jl/commit/dca0386673b055c889e21278a4a71a2d874c2577"
+        },
+        "date": 1750691484036,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "Jaynes–Cummings/First order Schrieffer-Wolf",
+            "value": 176857,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=51824\nallocs=1259\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":10,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "Micro Benchmarks/Nested commutator",
+            "value": 42491950,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=19656720\nallocs=463760\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":20,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "orjan.ameye@hotmail.com",
+            "name": "Orjan Ameye",
+            "username": "oameye"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8ee98c5320337b2b002e23b4338076abfc7ebe3e",
+          "message": "fix: Add/Mul(::Type{IndexedAverages} (#31)",
+          "timestamp": "2025-06-23T21:38:48+02:00",
+          "tree_id": "64044865f139e3c7feaa247819b6c4fe381e8181",
+          "url": "https://github.com/qojulia/SecondQuantizedAlgebra.jl/commit/8ee98c5320337b2b002e23b4338076abfc7ebe3e"
+        },
+        "date": 1750707637301,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "Jaynes–Cummings/First order Schrieffer-Wolf",
+            "value": 178002,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=51824\nallocs=1259\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":10,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "Micro Benchmarks/Nested commutator",
+            "value": 42300122,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=19656720\nallocs=463760\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":20,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "orjan.ameye@hotmail.com",
+            "name": "Orjan Ameye",
+            "username": "oameye"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "046953d88a99d36d5bc9ec6ef5b5023f5acd4f4a",
+          "message": "build: tag path release (#32)",
+          "timestamp": "2025-06-23T21:39:29+02:00",
+          "tree_id": "16c0cd4e0df08f1768ea9eafb33d142c2ec9721f",
+          "url": "https://github.com/qojulia/SecondQuantizedAlgebra.jl/commit/046953d88a99d36d5bc9ec6ef5b5023f5acd4f4a"
+        },
+        "date": 1750707675585,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "Jaynes–Cummings/First order Schrieffer-Wolf",
+            "value": 177191,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=51824\nallocs=1259\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":10,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "Micro Benchmarks/Nested commutator",
+            "value": 41677481,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=19656720\nallocs=463760\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":20,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
