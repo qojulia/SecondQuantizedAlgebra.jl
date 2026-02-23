c63edb1b9db11ff7d30f421193b29c0cffe385dc
christoph <chr.hotter@gmail.com>
2026-02-23 10:17:37 -0500

    format

--- test/test_numeric_conversion.jl
+++ test/test_numeric_conversion.jl
@@ -24,7 +24,8 @@ Random.seed!(0)
             for i in 1:3, j in 1:3
                 op = σ(i, j)
                 @test to_numeric(op, bnlevel) == QuantumOpticsBase.transition(bnlevel, i, j)
-                @test to_numeric(op, bnlevel, Dict()) == QuantumOpticsBase.transition(bnlevel, i, j)
+                @test to_numeric(op, bnlevel, Dict()) ==
+                    QuantumOpticsBase.transition(bnlevel, i, j)
             end
         end
 
