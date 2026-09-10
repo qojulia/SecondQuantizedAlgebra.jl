d318032e7337b1ea4d981e6178f43038781b6a43
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-10 20:41:20 +0200

    test: cover public formal unitary paths

--- test/algebra/operator_function_unitary_test.jl
+++ test/algebra/operator_function_unitary_test.jl
@@ -15,8 +15,20 @@ using Test
     @test @inferred(conjugate(sin(A), U)) == sin(image)
     @test @inferred(conjugate(expim(A), U)) == expim(image)
     @test conjugate(conjugate(cos(A), U), inv(U)) == cos(A)
+    @test transform(cos(A), U) == conjugate(cos(A), U)
+
+    composite = cos(A) + sin(A)
+    @test conjugate(composite, U) == cos(image) + sin(image)
 
     moving = Rotation(a, ω * t, t)
     @test transform(cos(A), moving) ==
         conjugate(cos(A), moving) + gauge_term(moving)
+
+    h2 = FockSpace(:left) ⊗ FockSpace(:right)
+    @qnumbers a2::Destroy(h2, 1) b2::Destroy(h2, 2)
+    A2 = a2 + a2'
+    mixing = Rotation(a2, b2, θ)
+    unit_coeff = cos(θ)^2 + sin(θ)^2
+    weighted = unit_coeff * cos(A2)
+    @test conjugate(weighted, mixing) == cos(conjugate(A2, mixing))
 end
