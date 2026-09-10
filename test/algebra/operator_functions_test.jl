87113c507d5c9ab3fcf33313458ea754ceef30d5
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-10 20:39:59 +0200

    test: cover public formal-expression algebra

--- test/algebra/operator_functions_test.jl
+++ test/algebra/operator_functions_test.jl
@@ -17,15 +17,33 @@ import SecondQuantizedAlgebra: QAdd, QExpr, expim
     @test sA isa QExpr
     @test @inferred((a + a')^4) isa QAdd
 
+    @test zero(cA) == zero(QExpr)
+    @test one(cA) == one(QExpr)
+    @test +cA === cA
+    @test iszero(cA * 0)
+
     @test 2 * cA + 3 * cA == 5 * cA
     @test iszero(cA - cA)
     @test cA + sA == sA + cA
+    @test cA + a == a + cA
+    @test 2 + cA == cA + 2
     @test one(QExpr) + 1 == 2 * one(QExpr)
     @test cA / 2 == (1 // 2) * cA
     @test cA // 2 == (1 // 2) * cA
 
     @test a * cA != cA * a
     @test (a * cA) * a == a * (cA * a)
+    @test cA * A isa QExpr
+    @test A * (A * cA) == (A * A) * cA
+
+    scalar_qadd = 2 * commutator(a, a')
+    @test cA * scalar_qadd == 2 * cA
+
+    @test cA^0 == one(QExpr)
+    @test cA^1 == cA
+    @test cA^3 == cA * cA * cA
+    @test_throws ArgumentError cA^(-1)
+    @test hash(cA) == hash(cos(A))
 
     @test @inferred(expim(A)) isa QExpr
     @test @inferred(expim(θ * A)) isa QExpr
@@ -34,6 +52,11 @@ import SecondQuantizedAlgebra: QAdd, QExpr, expim
     @test adjoint(adjoint(cA)) == cA
     @test adjoint(adjoint(sA)) == sA
     @test adjoint(adjoint(expim(A))) == expim(A)
+
+    formal_sum = cA + im * sA
+    @test adjoint(formal_sum) == adjoint(cA) + adjoint(im * sA)
+    formal_product = a * cA * a
+    @test adjoint(formal_product) == a' * adjoint(cA) * a'
 
     @test iszero(sin(zero(QExpr)))
     @test isone(cos(zero(QExpr)))
